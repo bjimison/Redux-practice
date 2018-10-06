275bc56235d69852a532c1c918536e1b01d0403c
bjimison <brady.t.jimison@gmail.com>
2018-10-06 11:56:14 -0700

    setup Cart container and subscribed it to Redux managed state

---
+++ shopping-cart/src/components/Shelf.js
@@ -0,0 +1,26 @@
+import React, { Componenet } from "react";
+
+class Shelf extends Componenet {
+  state = {
+    shelfItems: ["Bananas", "Frozen Pizza", "Flamin' Hot Cheetos", "Arugula"]
+  };
+
+  render() {
+    const shelfItems = this.state.shelfItems.map((item, id) => {
+      return (
+        <li key={id}>
+          {item}
+          <button onClick={() => this.props.addItem(item)}> + </button>
+        </li>
+      );
+    });
+    return (
+      <div>
+        <h2>Store Inventory</h2>
+        <ul>{shelfItems}</ul>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
---
+++ shopping-cart/src/containers/Cart.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import * as CartActions from "../actions/CartActions";
+import Shelf from "../components/Shelf";
+
+class Cart extends Component {
+  state = {};
+
+  render() {
+    const CartItems = this.props.cart.map((item, id) => {
+      return <li key={id}>{item}</li>;
+    });
+    return (
+      <div>
+        <Shelf addItem={this.props.actions.addToCart} />
+        <h2>Cart</h2>
+        <ol>{CartItems}</ol>
+      </div>
+    );
+  }
+}
+
+// Subscribes the container component to any changes in Redux-managed state
+// the state being mapped to props is the redux state
+function mapStateToProps(state, props) {
+  return {
+    cart: state.cart
+  };
+}
+
+// Changes in our program will be reflected when new actions are dispatched
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(CartActions, dispatch)
+  };
+}
+
+// returns a function wrapper that we need to pass the component into
+const wrapperFunction = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+// wraps the Cart component with the store connection configured above
+const wrappedComponent = wrapperFunction(Cart);
+
+export default wrappedComponent;
