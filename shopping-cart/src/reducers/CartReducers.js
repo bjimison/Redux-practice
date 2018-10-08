dbef0d31119cff240a33b021f8b554c212169e96
bjimison <brady.t.jimison@gmail.com>
2018-10-08 15:50:33 -0700

    added remove item action and reducer switch statement case fixed

--- shopping-cart/src/reducers/CartReducers.js
+++ shopping-cart/src/reducers/CartReducers.js
@@ -2,8 +2,6 @@ export default (state = [], action) => {
   switch (action.type) {
     case "ADD_ITEM":
       return [...state, action.item];
-    default:
-      return state;
     case "REMOVE_ITEM":
       return [...state];
     default:
